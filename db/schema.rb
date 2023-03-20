c0cba5b6407541d4cce4ac680a293e785be3c856
kidhri salim <salimkhidhri@gmail.com>
2023-03-20 18:37:42 +0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_191324) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_04_30_191324) do
   create_table "cheeses", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.boolean "is_best_seller"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
